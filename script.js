9c49a8ebff80433eee65a93db8a35f7529fe2e40
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2021-11-23 06:22:25 +0100

    allt klart förutom posta som lista på sidan

--- script.js
+++ script.js
@@ -9,7 +9,32 @@ let raknesatt = document.getElementById("raknesatt");
 
 myBtn.addEventListener ("click" , function(){
 
-    let summa = Number(firstNumber.value) + Number(secondNumber.value);
-    console.log(summa);
+    let summa = 0;
+    raknesatt = document.getElementById("raknesatt").value;
+    console.log(raknesatt);
+
+    switch(raknesatt) {
+        case "+":
+            summa = Number(firstNumber.value) + Number(secondNumber.value);
+            console.log(summa);
+            console.log("adderade");
+        break;
+        case "-":
+            summa = Number(firstNumber.value) - Number(secondNumber.value);
+            console.log(summa);
+            console.log("subtraherade");
+        break;
+        case "/":
+            summa = Number(firstNumber.value) / Number(secondNumber.value);
+            console.log(summa);
+            console.log("delade");
+        break;
+        case "*":
+            summa = Number(firstNumber.value) * Number(secondNumber.value);
+            console.log(summa);
+            console.log("gångrade");
+        break;
+
+    };
 
 });
