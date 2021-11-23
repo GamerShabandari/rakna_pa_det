067fdcf24e2348cad7e023530e851ef7d47bd3e7
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2021-11-23 05:54:34 +0100

    fixat det mesta förutom räkneval

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hejhejhej");
+
+
+let firstNumber = document.getElementById("firstNumber");
+
+let secondNumber = document.getElementById("secondNumber");
+
+let raknesatt = document.getElementById("raknesatt");
+
+
+myBtn.addEventListener ("click" , function(){
+
+    let summa = Number(firstNumber.value) + Number(secondNumber.value);
+    console.log(summa);
+
+});
